0c9ccf06ef57af264418e2d513cc419fc5e28372
ibiliskov <ivan.biliskov@gmail.com>
2014-02-17 10:33:54 +0100

    added test method, parsing options

--- dbtool/DatabaseOperations.cs
+++ dbtool/DatabaseOperations.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 
@@ -16,7 +17,17 @@ namespace dbtool
 
         public void TestConnection()
         {
-            
+            const int test = 3;
+            var connection = new SqlConnection(_options.ConnectionString);
+            var command = new SqlCommand("SELECT " + test, connection);
+            connection.Open();
+
+            var result = command.ExecuteScalar();
+
+            connection.Close();
+
+            if ((int) result != test)
+                throw new ArgumentException("Cannot connect to database.");
         }
 
         public void Backup()
--- dbtool/Program.cs
+++ dbtool/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 
@@ -7,8 +8,23 @@ namespace dbtool
 {
     class Program
     {
+        private static Options _options = null;
+
         static void Main(string[] args)
         {
+            _options = new Options
+            {
+                BackupFolder = ConfigurationManager.AppSettings.Get("backupFolder"),
+                ConnectionString = ConfigurationManager.AppSettings.Get("connectionString")
+            };
+
+            ParseCommand(args);
+        }
+
+        static void ParseCommand(string[] args)
+        {
+            var db = new DatabaseOperations(_options);
+            db.TestConnection();
         }
     }
 }
